9472189cff4569a908ec9e8893752035d0864f5f
annaliese-stone <stonea@allegheny.edu>
2022-11-14 13:30:07 -0500

    todos done but still getting errors

--- contactsearcher/contactsearcher/main.py
+++ contactsearcher/contactsearcher/main.py
@@ -1,9 +1,14 @@
 """Define the command-line interface for the contact searching program."""
 
-# TODO: Add all of the required import statements to this module
-
-# TODO: create a Typer object to support the command-line interface
-
+# doThis: Add all of the required import statements to this module
+from contactsearcher import search
+import typer
+from rich.console import Console
+from pathlib import Path
+from typing import Optional
+# doThis: create a Typer object to support the command-line interface
+cli = typer.Typer()
+console = Console()
 
 @cli.command()
 def contactsearcher(
@@ -30,12 +35,18 @@ def contactsearcher(
         typer.echo("The contacts file does not exist!")
     # display details about the search key for the job provided on the command line
     typer.echo("")
-    typer.echo(
-        f'  We are looking for contacts who have a job related to "{job_description}":'
-    )
-    # TODO: perform the search for all of the relevant email addresses given the job description
-    # TODO: we know that there are some contacts in the list, so iterate through the list of
+    typer.echo(f'  We are looking for contacts who have a job related to "{job_description}":')
+    # doThis: perform the search for all of the relevant email addresses given the job description
+    contacts = search.contacts
+    search.search_for_email_given_job(job_description, contacts)
+    # doThis: we know that there are some contacts in the list, so iterate through the list of
     # the contacts and display them in the terminal window
-    # TODO: display final information about the program's behavior in the terminal window;
+    # doThis: display final information about the program's behavior in the terminal window;
     # this should summarize whether or not the program found any matches
-    # TODO: refer to the expected output on Discord and/or Proactive Programmers for details
+    if search.search_for_email_given_job == True:
+        typer.echo(f'  {search.contacts, search.jobs}:')
+        typer.echo(f'')
+        typer.echo(f'Wow, we found some contacts! Email them to learn about your job!')
+    else:
+        typer.echo(f' No matches found :(')
+    # doThis: refer to the expected output on Discord and/or Proactive Programmers for details
--- contactsearcher/contactsearcher/search.py
+++ contactsearcher/contactsearcher/search.py
@@ -1,6 +1,7 @@
 """Search for an email address given a fragment of a job description."""
 
 from typing import List
+from contactsearcher import main
 
 import csv
 
@@ -9,11 +10,23 @@ import csv
 
 def search_for_email_given_job(job_description: str, contacts: str) -> List[List[str]]:
     """Search for and return job description(s) given an email address."""
-    # TODO: create an empty list of the contacts
-    # TODO: iterate through the file, parsing it line by line
-    # TODO: refer to the file called input/contacts.txt to learn more about
+    # doThis: create an empty list of the contacts
+    List = {}
+    # doThis: iterate through the file, parsing it line by line
+    # doThis: refer to the file called input/contacts.txt to learn more about
     # the format of the comma separated value (CSV) file that we must parse
-    # TODO: iterate through each line of the file and extract the current job
-    # ---> TODO: extract the current job for the contact on this line of the CSV
-    # ---> TODO: the job description matches and thus we should save it in the list
-    # TODO: return the list of the contacts who have a job description that matches
+    with open(main.contacts_file, newline='') as file:
+        file = csv.DictReader(main.contacts_file)
+        # doThis: iterate through each line of the file and extract the current job
+        # ---> doThis: extract the current job for the contact on this line of the CSV
+        for row in file:
+            print(row[contacts], row[job])
+            List[contacts] = [job]
+
+    # ---> doThis: the job description matches and thus we should save it in the list
+    for contacts, job in List:
+        if job == main.job_description:
+            # doThis: return the list of the contacts who have a job description that matches
+            return True
+        else:
+            return False
